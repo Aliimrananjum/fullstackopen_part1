0ff8ef0a10160f21825bd44eb676d2909d0245c0
Ali Imran Anjum <89576402+Aliimrananjum@users.noreply.github.com>
2022-08-18 10:17:22 +0200

    Merge branch 'master' of https://github.com/Aliimrananjum/fullstackopen_part1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 const Header = (props) =>{
   return(
     <div>
-      <h1>{props.course}</h1>
+      <p>{props.course}</p>
     </div>
   )
 }
@@ -9,9 +9,9 @@ const Header = (props) =>{
 const Content =(props) =>{
   return(
     <div>
-      <Part part={props.part1}/>
-      <Part part={props.part2}/>
-      <Part part={props.part3}/>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2}/>
+      <Part part={props.part3} exercises={props.exercises3}/>
     </div>
   ) 
 }
@@ -28,37 +28,32 @@ const Part = (props) =>{
   return(
     <div>
       <p>
-        {props.part.name} with {props.part.exercises} hours
+        {props.part} with {props.exercises} hours
       </p>
     </div>
   )
+ 
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <div>
       <Header course={course} />
-      <Content
-      part1={parts[0]} 
-      part2={parts[1]}
-      part3={parts[2]}/>
-      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises }/>
+      <Content part1={part1} part2={part2}
+      part3={part3}
+      
+      exercises1={exercises1}
+      exercises2={exercises2}
+      exercises3={exercises3}/>
+      <Total total={exercises1+exercises2+exercises3} />
     </div>
   )
 }
