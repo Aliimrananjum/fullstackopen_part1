4e546527e3238370bfa6390a01b2bb3114930be0
Ali Imran Anjum <89576402+Aliimrananjum@users.noreply.github.com>
2022-08-19 09:40:22 +0200

    Starting on 1.8: unicafe step3

--- src/App.js
+++ src/App.js
@@ -1,67 +1,21 @@
-const Header = (props) =>{
-  return(
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
+import { useState } from 'react'
 
-const Content =(props) =>{
-  return(
-    <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
-    </div>
-  ) 
-}
-
-const Total =(props) =>{
-  return(
-    <div>
-      <p>Total hours {props.total} </p>
-    </div>
-  )
-}
-
-const Part = (props) =>{
-  return(
-    <div>
-      <p>
-        {props.part.name} with {props.part.exercises} hours
-      </p>
-    </div>
-  )
- 
+const Statistics = (props) => {
+  // ...
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  // do not define a component within another component
+  const Statistics = (props) => {
+    // ...
   }
 
-
   return (
-    <div>
-      <Header course={course.name} />
-      <Content part1={course.parts[0]} part2={course.parts[1]}
-      part3={course.parts[2]}/>
-      <Total total={course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises} />
-    </div>
+    // ...
   )
 }
 
