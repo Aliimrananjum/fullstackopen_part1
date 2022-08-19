4db521b8c7da3ebed35ea1be6839b500e15b6450
Ali Imran Anjum <89576402+Aliimrananjum@users.noreply.github.com>
2022-08-19 11:09:46 +0200

    1.8: unicafe step3 completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import { useState } from 'react'
 
-const Statistics = (props) => {
-  // ...
+const Button = ({onClick, text}) =>{
+  return(
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+  const average = ((good - bad)/total)
+  const prosent =((good/total)*100)
+  return(
+
+    <div>
+      <h1>Statistics</h1>
+
+    <p>Good {good}</p>
+    <p>Neutral {neutral}</p>
+    <p>Bad {bad}</p>
+    <p>Total {total}</p>
+    <p>Average {average}</p>
+    <p>Prosent {prosent} %</p>
+    </div>
+
+  )
 }
 
 const App = () => {
@@ -9,13 +32,18 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  // do not define a component within another component
-  const Statistics = (props) => {
-    // ...
-  }
+  const increment = (state,setState) =>() => setState(state +1)
+
 
   return (
-    // ...
+    <div>
+      <h1>Give feedback</h1>
+      <Button onClick={increment(good,setGood)} text="Good"/>
+      <Button onClick={increment(neutral,setNeutral)} text="Neutral"/>
+      <Button onClick={increment(bad,setBad)} text="Bad"/>
+    <Statistics good={good} neutral={neutral} bad={bad}/>
+  
+    </div>
   )
 }
 
